44b453b7fe43ba7491eb6cef4ba530e3ee5f6eb1
Thanh Tran <thuthanhtran2610@gmail.com>
2022-11-17 21:04:29 +0700

    [CMR-59] Recover User

--- src/users/users.router.ts
+++ src/users/users.router.ts
@@ -6,7 +6,9 @@ import {
     getUsers,
     getUser,
     changePassword,
-    changePosition
+    changePosition,
+    getLockedUsers,
+    recoverUser
 } from './users.controller';
 import { authorization } from '../core/middleware/auth.middleware'
 import { Roles } from '../core/enum';
@@ -14,11 +16,13 @@ import { Roles } from '../core/enum';
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/locked', getLockedUsers);
 router.post('/', authorization(Roles.ADMIN, Roles.MANAGER), createUser);
 router.put('/me/password', authorization(), changePassword);
 
 router.get('/:id', getUser);
 router.put('/:id', authorization(Roles.ADMIN, Roles.MANAGER), updateUser);
+router.put('/locked/:id', authorization(Roles.ADMIN, Roles.MANAGER), recoverUser);
 router.delete('/:id', authorization(Roles.ADMIN, Roles.MANAGER), deleteUser);
 router.put('/position/:id', authorization(Roles.ADMIN), changePosition);
 
