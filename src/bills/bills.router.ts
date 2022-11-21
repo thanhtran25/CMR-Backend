effe9a06fb6539533df8fec64348d9429ad0d356
Thanh Tran <thuthanhtran2610@gmail.com>
2022-11-21 19:37:35 +0700

    [CMR-64] Fix router

--- src/bills/bills.router.ts
+++ src/bills/bills.router.ts
@@ -10,17 +10,17 @@ import {
 } from './bills.controller';
 import { authorization } from '../core/middleware/auth.middleware'
 import { Roles } from '../core/enum';
+import { ANONYMOUS_USER } from '../core/constant';
 
 const router = express.Router();
 
 router.get('/', authorization(Roles.MANAGER, Roles.STAFF), getBills);
+router.post('/', authorization(ANONYMOUS_USER), createBill);
+router.post('/shipping', getShippingFee)
 router.get('/history', authorization(Roles.CUSTOMER), getHistory);
-router.post('/', createBill);
 
-router.get('/:id', authorization(Roles.MANAGER, Roles.STAFF, Roles.CUSTOMER), getBill);
 router.put('/accept/:id', authorization(Roles.MANAGER, Roles.STAFF), acceptBill);
 router.put('/ship/:id', authorization(Roles.SHIPPER), shipping)
-
-router.post('/shipping', getShippingFee)
+router.get('/:id', authorization(Roles.MANAGER, Roles.STAFF, Roles.CUSTOMER), getBill);
 
 export default router;
